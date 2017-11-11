cd73b5e9434cc2d14b2316d81d0f537b41f255d8
Lucas Lencinas <lllencinas@gmail.com>
2017-11-10 21:37:10 -0300

    checkpoint-4

--- schema.js
+++ schema.js
@@ -44,53 +44,6 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
-// Mutations
-const mutation = new GraphQLObjectType({
-  name: 'Mutation',
-  fields: {
-    addPlayer: {
-      type: PlayerType,
-      args: {
-        name: {type: new GraphQLNonNull(GraphQLString)},
-        age: {type: new GraphQLNonNull(GraphQLInt)},
-        position: {type: new GraphQLNonNull(GraphQLString)},
-      },
-      resolve(parentValue,args) {
-        return axios.post('http://localhost:3000/players', {
-          name: args.name,
-          age: args.age,
-          position: args.position
-        })
-        .then(res => res.data)
-      }
-    },
-    deletePlayer: {
-      type: PlayerType,
-      args: {
-        id: {type: new GraphQLNonNull(GraphQLString)},
-      },
-      resolve(parentValue,args) {
-        return axios.delete('http://localhost:3000/players/' + args.id)
-        .then(res => res.data)
-      }
-    },
-    updatePlayer: {
-      type: PlayerType,
-      args: {
-        id: {type: new GraphQLNonNull(GraphQLString)},
-        name: {type:GraphQLString},
-        age: {type:GraphQLInt},
-        position: {type:GraphQLString}
-      },
-      resolve(parentValue,args) {
-        return axios.patch('http://localhost:3000/players/' + args.id)
-        .then(res => res.data)
-      }
-    }
-  }
-})
-
 module.exports = new GraphQLSchema({
-  query: RootQuery,
-  mutation
+  query: RootQuery
 });
